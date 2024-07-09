074c9e490a9e589f0c14d6a7ef475bc935c5fa16
fg12111 <github@02n.net>
2024-07-09 22:04:14 +0200

    Introduce PriceHistoryService

---
+++ src/main/java/com/my/goldmanager/service/PriceHistoryService.java
@@ -0,0 +1,52 @@
+package com.my.goldmanager.service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.my.goldmanager.entity.Item;
+import com.my.goldmanager.entity.MaterialHistory;
+import com.my.goldmanager.repository.ItemRepository;
+import com.my.goldmanager.repository.MaterialHistoryRepository;
+import com.my.goldmanager.rest.entity.Price;
+import com.my.goldmanager.rest.entity.PriceHistory;
+import com.my.goldmanager.rest.entity.PriceHistoryList;
+import com.my.goldmanager.rest.entity.PriceList;
+
+@Service
+
+public class PriceHistoryService {
+
+	@Autowired
+	private MaterialHistoryRepository materialHistoryRepository;
+	@Autowired
+	private ItemRepository itemRepository;
+
+	public PriceHistoryList listAllForMaterial(String materialId) {
+		List<Item> items = itemRepository.findByMaterialId(materialId);
+		PriceHistoryList result = new PriceHistoryList();
+		result.setPriceHistories(new LinkedList<>());
+		if (!items.isEmpty()) {
+			List<MaterialHistory> historyList = materialHistoryRepository.findByMaterial(materialId);
+			for (MaterialHistory history : historyList) {
+
+				PriceHistory priceHistory = new PriceHistory();
+				priceHistory.setDate(history.getEntryDate());
+				PriceList priceList = new PriceList();
+				priceHistory.setPriceList(priceList);
+				result.getPriceHistories().add(priceHistory);
+				priceList.setPrices(new LinkedList<>());
+				for (Item item : items) {
+					Price price = new Price();
+					price.setItem(item);
+					price.setPrice(PriceCalculatorUtil.claculatePrice(item, history.getPrice()));
+					priceList.getPrices().add(price);
+					priceList.setTotalPrize(priceList.getTotalPrize() + price.getPrice());
+				}
+			}
+		}
+		return result;
+	}
+}
